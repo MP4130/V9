80854e892b432510b56fd725075a2f2b11a0e676
MP4130 <rowy2025@gmail.com>
2026-08-25 08:45:29 +0200

    3

--- layers/entitiesL_6_init.js
+++
@@ -1,1 +0,0 @@
-var json_entitiesL_6 = {"type":"FeatureCollection","name":"entitiesL_6","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"xy_coordinate_resolution":1e-15,"features":[]}
